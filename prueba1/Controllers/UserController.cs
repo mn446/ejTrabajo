3694951e2827d020630201c52d6539bb7b7da66f
Martin <mnavarro@corp.travelgenio.com>
2017-10-10 14:11:46 -0700

    quedo andando el ABMC

---
+++ prueba1/Controllers/UserController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prueba1.Models;
+
+namespace prueba1.Controllers
+{
+    public class UserController : Controller
+    {
+        private ModelContext db = new ModelContext();
+
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View(db.usuarios.ToList());
+        }
+
+        //
+        // GET: /User/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            Usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /User/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuarios.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        //
+        // GET: /User/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            Usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /User/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /User/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            Usuario usuario = db.usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /User/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Usuario usuario = db.usuarios.Find(id);
+            usuario.activo = false; // baja lógica
+            //db.usuarios.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
